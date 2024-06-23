e434143ba5feaa2bfa707e70f20656e9ff77b151
thekilla0 <omar.djebara14@gmail.com>
2024-06-23 13:46:11 +0100

    front and mix laravel

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,10 +2,13 @@ const mix = require("laravel-mix");
 
 mix.js("resources/js/app.js", "public/js")
     .sass("resources/sass/app.scss", "public/css")
-    .sourceMaps();
-
-mix.browserSync("localhost:8000");
-
-mix.options({
-    processCssUrls: false,
-});
+    .options({
+        processCssUrls: false,
+    })
+    .sourceMaps()
+    .webpackConfig({
+        stats: {
+            children: true,
+            errorDetails: true,
+        },
+    });
